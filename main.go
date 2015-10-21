3c6e81c520c31cf4268b7b7cd4a95f94f2e1529c
Miles Crabill <mcrabill@mozilla.com>
2015-10-20 20:14:41 -0700

    started on s3 upload

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/defaults"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/satori/go.uuid"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
@@ -12,17 +19,37 @@ import (
 	"github.com/xyproto/permissions2"
 )
 
+var s3Client *s3.S3
+var uploader *s3manager.Uploader
+
+const logs_bucket = "webrtc_logs"
+
 var RootHandler = func(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "root")
 }
 
 var DropHandler = func(w http.ResponseWriter, req *http.Request) {
-	if ungzippedBody, err := gzip.NewReader(req.Body); err == nil {
-		if contents, err := ioutil.ReadAll(ungzippedBody); err == nil {
-			fmt.Fprintln(w, string(contents))
-		}
+	if ungzippedBody, err := gzip.NewReader(req.Body); err != nil {
+		http.Error(w, "Could not read logs.", http.StatusInternalServerError)
 	} else {
-		http.Error(w, "Could not read file contents.", http.StatusInternalServerError)
+		if _, err := ioutil.ReadAll(ungzippedBody); err != nil {
+			http.Error(w, "Could not decode logs.", http.StatusInternalServerError)
+		} else {
+			// generate uuid
+			uuid := uuid.NewV4()
+
+			result, err := uploader.Upload(&s3manager.UploadInput{
+				Bucket:          aws.String(logs_bucket),
+				Key:             aws.String(fmt.Sprintf("%s-%s", uuid, time.Now().Format(time.RFC3339))),
+				Body:            req.Body,
+				ContentEncoding: aws.String("gzip"),
+			})
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Could not upload logs. Error: %s", err), http.StatusInternalServerError)
+			} else {
+				fmt.Fprintln(w, fmt.Sprintf("Upload success, uuid: %, result: %s", uuid, result))
+			}
+		}
 	}
 }
 
@@ -30,19 +57,38 @@ var DeniedFunction = func(w http.ResponseWriter, req *http.Request) {
 	http.Error(w, "Permission denied!", http.StatusForbidden)
 }
 
+func init() {
+	// default aws region
+	defaults.DefaultConfig.Region = aws.String("us-west-2")
+
+	// s3
+	s3Client = s3.New(nil)
+
+	// make sure bucket exists
+	result, err := s3Client.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(logs_bucket),
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.GoString())
+}
+
 func main() {
+	// s3
+	uploader = s3manager.NewUploader(nil)
+
 	// gorilla mux
-	r := mux.NewRouter().StrictSlash(false)
-	r.HandleFunc("/", RootHandler)
-	r.HandleFunc("/drop", DropHandler).
+	router := mux.NewRouter().StrictSlash(false)
+	router.HandleFunc("/", RootHandler)
+	router.HandleFunc("/drop", DropHandler).
 		Methods("POST").
 		HeadersRegexp("Content-Type", "application/gzip")
 
 	// negroni
-	n := negroni.Classic()
+	neg := negroni.Classic()
 
 	// middleware instantiations
-
 	// requires redis, default port is 6379
 	perm := permissions.New()
 
@@ -50,11 +96,10 @@ func main() {
 	perm.SetDenyFunction(DeniedFunction)
 
 	// middleware
-	n.Use(perm)
-	n.Use(negroni_gzip.Gzip(negroni_gzip.DefaultCompression))
+	neg.Use(perm)
+	neg.Use(negroni_gzip.Gzip(negroni_gzip.DefaultCompression))
 
 	// handlers
-	n.UseHandler(r)
-
-	n.Run(":8080")
+	neg.UseHandler(router)
+	neg.Run(":8080")
 }
